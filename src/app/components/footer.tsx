f94609870a4268d7035de00cfe2ffbcde4277586
davkooz <kuzmicz.dawid@gmail.com>
2023-08-19 23:23:15 +0200

    1.1version

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -6,33 +6,33 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <main className="bg-white overflow-x-hidden flex flex-col mt-8 border-t-2 pt-8">
-            <div className="flex justify-between">
-                <div className="flex flex-row gap-4 ml-12">
+            <div className="flex justify-between px-4 md:px-12">
+                <div className="flex flex-row gap-2 md:gap-4">
                     <Link href="https://www.facebook.com/NatalliaFineArtPhotography" target='_blank'>
-                    <Image src="/icons/facebook.svg" alt="facebookicon" width={24} height={24}/>
+                    <Image src="/icons/facebook.svg" alt="facebookicon" width={32} height={32} className="md:w-6 md:h-6"/>
                     </Link>
                     <Link href="https://www.instagram.com/natallia.wyszkowska/" target='_blank'>
-                    <Image src="/icons/instagram.svg" alt="instagramicon" width={24} height={24}/>
+                    <Image src="/icons/instagram.svg" alt="instagramicon" width={32} height={32} className="md:w-6 md:h-6"/>
                     </Link>
                     <Link href="https://www.tiktok.com/@natalliawyszkowska" target='_blank'>
-                    <Image src="/icons/tiktok.svg" alt="tiktokicon" width={24} height={24}/>
+                    <Image src="/icons/tiktok.svg" alt="tiktokicon" width={32} height={32} className="md:w-6 md:h-6"/>
                     </Link>
                 </div>
-                <div className="flex flex-row mr-12">
+                <div className="flex flex-row gap-2 md:gap-4">
                     <Link href="/regulamin">
-                        <p className="text-s block text-black hover:text-gray-400 mr-2 underline underline-offset-1">
+                        <p className="text-s block text-black hover:text-gray-400 underline underline-offset-1">
                             Regulamin
                         </p>
                     </Link>
                     <Link href="/politykaprywatnosci">
-                        <p className="text-s block text-black hover:text-gray-400 ml-2 underline underline-offset-1">
+                        <p className="text-s block text-black hover:text-gray-400 underline underline-offset-1">
                             Polityka prywatności
                         </p>
                     </Link>
                 </div>
             </div>
             <div className="flex pt-4 justify-center">
-                <p className=" text-black text-xs">&copy;NATALLIAWYSZKOWSKA.COM | 2023 | PROJEKT: DAWID KUZMICZ</p>
+                <p className=" text-black text-center text-xs">&copy;NATALLIAWYSZKOWSKA.COM | 2023 | PROJEKT: DAWID KUZMICZ</p>
             </div>
         </main>
     );
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,3 @@
-// components/Navbar.tsx
 'use client';
 
 import { useState } from 'react';
@@ -14,13 +13,13 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex items-center overflow-x-hidden justify-between flex-wrap bg-white border-b-2 mb-8 sm:justify-center">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <nav className="flex items-center overflow-x-hidden justify-center flex-wrap bg-white border-b-2 mb-8">
+      <div className="mx-auto">
         <Link href="/">
-        <Image src="/logo2.png" alt="Natallia Wyszkowska Logo" width={400} height={284} />
+          <Image src="/logo2.png" alt="Natallia Wyszkowska Logo" width={400} height={284} />
         </Link>
       </div>
-      <div className="block lg:hidden">
+      <div className="block lg:hidden mx-auto">
         <button
           onClick={handleToggle}
           className="flex items-center px-3 py-2 border rounded text-gray-400 border-black hover:text-gray-400 hover:border-gray-400"
@@ -38,46 +37,46 @@ const Navbar = () => {
       <div
         className={`${
           isOpen ? 'block' : 'hidden'
-        } w-full block flex-grow lg:flex lg:items-center lg:w-auto`}
+        } w-full block flex-grow lg:flex lg:items-center lg:w-auto justify-center`}
       >
-        <div className="ml-4 text-md lg:flex-grow">
+        <div className="text-md lg:flex-grow flex flex-col lg:flex-row items-center">
           <Link href="/">
-            <p className="block mt-4 lg:inline-block text-gray-400 hover:text-black mr-4">
+            <p className="mt-4 lg:inline-block text-gray-400 hover:text-black mx-4">
               Strona główna
             </p>
           </Link>
           <Link href="/omnie">
-            <p className="block mt-4 lg:inline-block text-gray-400 hover:text-black mr-4">
+            <p className="mt-4 lg:inline-block text-gray-400 hover:text-black mx-4">
               O mnie
             </p>
           </Link>
           <Link href="/oferta">
-            <p className="block mt-4 lg:inline-block text-gray-400 hover:text-black mr-4">
+            <p className="mt-4 lg:inline-block text-gray-400 hover:text-black mx-4">
               Oferta
             </p>
           </Link>
           <Link href="/portfolio">
-            <p className="block mt-4 lg:inline-block text-gray-400 hover:text-black mr-4">
+            <p className="mt-4 lg:inline-block text-gray-400 hover:text-black mx-4">
               Portfolio
             </p>
           </Link>
           <Link href="/kontakt">
-            <p className="block mt-4 mb-4 lg:inline-block text-gray-400 hover:text-black mr-4">
+            <p className="mt-4 lg:inline-block text-gray-400 hover:text-black mx-4">
               Kontakt
             </p>
           </Link>
         </div>
-        <div className="flex flex-row gap-8 mr-8">
-                    <Link href="https://www.facebook.com/NatalliaFineArtPhotography" target='_blank'>
-                    <Image src="/icons/facebook.svg" alt="facebookicon" width={28} height={28}/>
-                    </Link>
-                    <Link href="https://www.instagram.com/natallia.wyszkowska/" target='_blank'>
-                    <Image src="/icons/instagram.svg" alt="instagramicon" width={28} height={28}/>
-                    </Link>
-                    <Link href="https://www.tiktok.com/@natalliawyszkowska" target='_blank'>
-                    <Image src="/icons/tiktok.svg" alt="tiktokicon" width={28} height={28}/>
-                    </Link>
-                </div>
+        <div className="flex flex-row gap-8 mb-4 mt-6 mx-auto justify-center">
+          <Link href="https://www.facebook.com/NatalliaFineArtPhotography" target='_blank'>
+            <Image src="/icons/facebook.svg" alt="facebookicon" width={28} height={28}/>
+          </Link>
+          <Link href="https://www.instagram.com/natallia.wyszkowska/" target='_blank'>
+            <Image src="/icons/instagram.svg" alt="instagramicon" width={28} height={28}/>
+          </Link>
+          <Link href="https://www.tiktok.com/@natalliawyszkowska" target='_blank'>
+            <Image src="/icons/tiktok.svg" alt="tiktokicon" width={28} height={28}/>
+          </Link>
+        </div>
       </div>
     </nav>
   );
