dd7495ed8bd6a3ce823b1690b4188c1a37375a61
davkooz <kuzmicz.dawid@gmail.com>
2023-08-19 22:45:24 +0200

    bug fixes

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -7,41 +7,41 @@ import Link from "next/link";
 
 const Kontakt: React.FC = () => {
     return (
-        <main className="bg-white min-w-screen min-h-screen flex flex-col px-4">
+        <main className="bg-white min-h-screen flex flex-col px-4">
             <Navbar />
             
             <div className="flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-16 justify-center items-center mt-6">
-                <div className="flex flex-col space-y-8">
-                    <div className="flex items-center space-x-8">
+                <div className="flex flex-col space-y-6 md:space-y-8">
+                    <div className="flex items-center space-x-4 md:space-x-8">
                         <Image src="/icons/email.svg" alt="emailicon" width={32} height={32} />
                         <Link href="mailto:natalliafineart@gmail.com">
-                            <p className="text-xl cursor-pointer">natalliafineart@gmail.com</p>
+                            <div className="text-lg md:text-xl cursor-pointer">natalliafineart@gmail.com</div>
                         </Link>
                     </div>
-                    <div className="flex items-center space-x-8">
+                    <div className="flex items-center space-x-4 md:space-x-8">
                         <Image src="/icons/phone.svg" alt="phoneicon" width={32} height={32} />
-                        <p className="text-xl">+48 690 976 993</p>
+                        <div className="text-lg md:text-xl">+48 690 976 993</div>
                     </div>
-                    <div className="flex items-center space-x-8">
+                    <div className="flex items-center space-x-4 md:space-x-8">
                         <Image src="/icons/location.svg" alt="locationicon" width={32} height={32} />
-                        <p className="text-xl">Warszawa - Mińsk Mazowiecki</p>
+                        <div className="text-lg md:text-xl">Warszawa - Mińsk Mazowiecki</div>
                     </div>
                     <Link href="https://www.instagram.com/natallia.wyszkowska/" target='_blank'>
-                        <div className="flex items-center space-x-8 cursor-pointer">
+                        <div className="flex items-center space-x-4 md:space-x-8 cursor-pointer">
                             <Image src="/icons/instagram.svg" alt="instagramicon" width={32} height={32} />
-                            <p className="text-xl">natallia.wyszkowska</p>
+                            <div className="text-lg md:text-xl">natallia.wyszkowska</div>
                         </div>
                     </Link>
                     <Link href="https://www.facebook.com/NatalliaFineArtPhotography" target='_blank'>
-                        <div className="flex items-center space-x-8 cursor-pointer">
+                        <div className="flex items-center space-x-4 md:space-x-8 cursor-pointer">
                             <Image src="/icons/facebook.svg" alt="facebookicon" width={32} height={32} />
-                            <p className="text-xl">Natallia Wyszkowska - Fotografia Film</p>
+                            <div className="text-lg md:text-xl">Natallia Wyszkowska - Fotografia Film</div>
                         </div>
                     </Link>
                 </div>
                 
                 <div className="mt-8 md:mt-0">
-                    <Image src="/tosialod.jpg" alt="tosialod" width={600} height={600} className="rounded" />
+                    <Image src="/tosialod.jpg" alt="tosialod" width={300} height={300} className="rounded md:w-[600px] md:h-[600px]" />
                 </div>
             </div>
             
