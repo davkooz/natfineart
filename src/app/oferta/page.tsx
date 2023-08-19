c516fbc5cf883d9b21b250a8f8ada69324c784ec
davkooz <kuzmicz.dawid@gmail.com>
2023-08-19 23:05:01 +0200

    test

--- src/app/oferta/page.tsx
+++ src/app/oferta/page.tsx
@@ -10,7 +10,7 @@ const Oferta = () => {
         <main className="bg-white overflow-x-hidden min-h-screen flex flex-col">
             <Navbar />
             <div className="flex mb-8 w-full md:w-[44rem] self-center px-4">
-                <p className="text-center text-l">
+                <p className="text-center text-l sm:text-s">
                     Zdjęcia są w formie elektronicznej + wydruki 15×21 cm na papierze jedwabistym.
                     <br />Istnieje możliwość dokupienia dodatkowych ujęć 50 zł/sztuka.
                     <br />Sesje fotograficzne odbywają się w profesjonalnie wyposażonym studio lub w plenerze i trwają – w
@@ -25,13 +25,17 @@ const Oferta = () => {
                     { title: "Film z Sesji", price: "1500,00 zł", items: ["Czas trwania filmu to 3-4 minuty"] },
                     { title: "Klip/Reels", price: "500,00 zł", items: ["Idealny do social media"] }
                 ].map((pakiet, index) => (
-                    <motion.div whileHover={{ y: 6, transition: { duration: 0.3 } }} key={index} className="flex flex-col items-center w-full md:w-96 border-2 border-black rounded-2xl gap-6 p-8 m-2">
-                        <h1 className="font-bold text-3xl text-black text-center">{pakiet.title}</h1>
+                    <motion.div 
+                        whileHover={{ y: 6, transition: { duration: 0.3 } }} 
+                        key={index} 
+                        className="flex flex-col items-center w-full md:w-96 border-2 border-black rounded-2xl gap-6 p-4 md:p-8 m-2"
+                    >
+                        <h1 className="font-bold text-2xl md:text-3xl text-black text-center">{pakiet.title}</h1>
                         <hr className="border-black border-1 rounded-full w-32" />
-                        <h2 className="font-bold text-2xl">{pakiet.price}</h2>
+                        <h2 className="font-bold text-xl md:text-2xl">{pakiet.price}</h2>
                         <ul className="list-disc space-y-1 text-left">
                             {pakiet.items.map((item, itemIndex) => (
-                                <li key={itemIndex} className="text-xl">{item}</li>
+                                <li key={itemIndex} className="text-lg md:text-xl">{item}</li>
                             ))}
                         </ul>
                         <div className="flex justify-center">
